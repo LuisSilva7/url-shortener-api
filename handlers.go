80f45749aad7e086e3c20fdef410e53cee2c39e3
LuisSilva7 <lmpsilva7@gmail.com>
2025-02-03 10:13:30 +0000

    added shorten endpoint

--- handlers.go
+++ handlers.go
@@ -1,1 +1,62 @@
 package main
+
+import (
+	"context"
+	"encoding/json"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+type UrlRequest struct {
+	LongUrl     string `json:"long_url"`
+	CustomAlias string `json:"custom_alias"`
+	Expiration  int    `json:"expiration"`
+}
+
+func ShortenHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var req UrlRequest
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "Error parsing request body", http.StatusBadRequest)
+		return
+	}
+
+	shortID := req.CustomAlias
+	if shortID == "" {
+		shortID = generateShortID(5)
+	}
+
+	ctx := context.Background()
+
+	expirationTime := time.Duration(req.Expiration) * time.Second
+	err := RedisClient.Set(ctx, shortID, req.LongUrl, expirationTime).Err()
+	if err != nil {
+		http.Error(w, "Failed to store URL in Redis", http.StatusInternalServerError)
+		return
+	}
+
+	response, err := json.Marshal(map[string]string{"short_url": shortID})
+	if err != nil {
+		http.Error(w, "Error marshaling response", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
+}
+
+func generateShortID(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(b)
+}
--- routes.go
+++ routes.go
@@ -5,28 +5,23 @@ import (
 	"strings"
 )
 
-// SetupRoutes initializes the HTTP routes and returns a ServeMux
 func SetupRoutes() *http.ServeMux {
 	mux := http.NewServeMux()
 
-	// Route for shortening URLs
 	mux.HandleFunc("/shorten", ShortenHandler)
 
-	// Dynamic route handling for shortened URLs and stats
 	mux.HandleFunc("/", DynamicHandler)
 
 	return mux
 }
 
-// DynamicHandler handles requests that depend on URL parameters
 func DynamicHandler(w http.ResponseWriter, r *http.Request) {
 	path := strings.TrimPrefix(r.URL.Path, "/")
 
-	// Check if the path matches a shortened URL or a stats request
 	if strings.HasPrefix(path, "stats/") {
-		StatsHandler(w, r)
+		// StatsHandler(w, r)
 	} else {
-		RedirectHandler(w, r)
+		// RedirectHandler(w, r)
 	}
 }
 
